7e0b6ad5280ffae361051b6c7114ff7bb4d501bb
syukrianuar <2018638988@student.uitm.edu.my>
2021-10-29 14:51:52 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/getfromapi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getfromapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const DataFromAPI(),      
+    );
+  }
+}
+
+class DataFromAPI extends StatefulWidget {
+  const DataFromAPI({ Key? key }) : super(key: key);
+  
+  @override
+  _DataFromAPIState createState() => _DataFromAPIState();
+}
+
+class _DataFromAPIState extends State<DataFromAPI> {
+  getUserData() async{
+    var response = await http.get(Uri.https('jsonplaceholder.typicode.com', 'users'));
+    var jsonData = jsonDecode(response.body);
+    List<User> users = [];
+
+    for(var u in jsonData){
+      User user = User(u["name"], u["email"], u["userName"]);
+      users.add(user);
+    }
+    print(users.length);
+    return users;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Data'),
+      ),
+      body: Container(
+        child: Card(child: FutureBuilder(
+          future: getUserData(),
+          builder: (context, snapshot){
+            if(snapshot.data == null){
+              return Container(
+                child: Center(
+                  child: Text('Loading...'),
+                ),
+              );
+            }else 
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, i){
+                return ListTile(
+                  title: Text(snapshot.data[i].name),
+                  subtitle: Text(snapshot.data[i].userName),
+                  trailing: Text(snapshot.data[i].email),
+                );
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class User{
+  final String name, email, userName;
+  User(this.name, this.email, this.userName);
+}
+
